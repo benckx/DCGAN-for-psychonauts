03c5dfdaa4cb4d2c6384dbb48ea2ad5a61dba0b7
benckx <benoit.vleminckx@gmail.com>
2019-03-19 15:26:37 +0100

    disable async ftp as something doesn't seem to work

--- files_utils.py
+++ files_utils.py
@@ -6,12 +6,12 @@ import os.path
 import os.path
 import time
 import zipfile
-from multiprocessing import Pool
+# from multiprocessing import Pool
 
-ftp_threads_pool = Pool(processes=10)
+# ftp_threads_pool = Pool(processes=10)
 
 
-def upload_via_ftp_sync(file_name):
+def upload_via_ftp(file_name):
   print('uploading {} to ftp'.format(file_name))
   try:
     if os.path.isfile('ftp.ini'):
@@ -29,12 +29,12 @@ def upload_via_ftp_sync(file_name):
     print('Error during FTP transfer of file {} -> {}'.format(file_name, e))
 
 
-def upload_via_ftp(file_name):
-  try:
-    print('Before adding {} to ftp thread pool'.format(file_name))
-    ftp_threads_pool.apply_async(upload_via_ftp_sync, args=[file_name])
-  except Exception as e:
-    print('Error during FTP thread pool queuing of file {} -> {}'.format(file_name, e))
+# def upload_via_ftp(file_name):
+#   try:
+#     print('Before adding {} to ftp thread pool'.format(file_name))
+#     ftp_threads_pool.apply_async(upload_via_ftp_sync, args=[file_name])
+#   except Exception as e:
+#     print('Error during FTP thread pool queuing of file {} -> {}'.format(file_name, e))
 
 
 def zip_folder(folder, zip_file_name):
