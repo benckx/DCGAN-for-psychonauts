282fe7b9764b967115fab94d66e50c138a6af8a5
benckx <benoit.vleminckx@gmail.com>
2019-03-22 17:39:55 +0100

    spread variables across gpus

--- ops.py
+++ ops.py
@@ -1,9 +1,3 @@
-import math
-import numpy as np
-import tensorflow as tf
-
-from tensorflow.python.framework import ops
-
 from utils import *
 
 try:
@@ -53,44 +47,46 @@ def conv_cond_concat(x, y):
     x, y*tf.ones([x_shapes[0], x_shapes[1], x_shapes[2], y_shapes[3]])], 3)
 
 
-def conv2d(input_, output_dim,
+def conv2d(input_, output_dim, device_num,
        k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
        name="conv2d"):
-  with tf.variable_scope(name):
-    w = tf.get_variable('w', [k_h, k_w, input_.get_shape()[-1], output_dim],
-              initializer=tf.truncated_normal_initializer(stddev=stddev))
-    conv = tf.nn.conv2d(input_, w, strides=[1, d_h, d_w, 1], padding='SAME')
+  with tf.device("/device:GPU:{}".format(device_num)):
+    with tf.variable_scope(name):
+      w = tf.get_variable('w', [k_h, k_w, input_.get_shape()[-1], output_dim],
+                initializer=tf.truncated_normal_initializer(stddev=stddev))
+      conv = tf.nn.conv2d(input_, w, strides=[1, d_h, d_w, 1], padding='SAME')
 
-    biases = tf.get_variable('biases', [output_dim], initializer=tf.constant_initializer(0.0))
-    conv = tf.reshape(tf.nn.bias_add(conv, biases), conv.get_shape())
+      biases = tf.get_variable('biases', [output_dim], initializer=tf.constant_initializer(0.0))
+      conv = tf.reshape(tf.nn.bias_add(conv, biases), conv.get_shape())
 
-    return conv
+      return conv
 
 
-def deconv2d(input_, output_shape,
+def deconv2d(input_, output_shape, device_num,
        k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
        name="deconv2d", with_w=False):
-  with tf.variable_scope(name):
-    # filter : [height, width, output_channels, in_channels]
-    w = tf.get_variable('w', [k_h, k_w, output_shape[-1], input_.get_shape()[-1]],
-              initializer=tf.random_normal_initializer(stddev=stddev))
+  with tf.device("/device:GPU:{}".format(device_num)):
+    with tf.variable_scope(name):
+      # filter : [height, width, output_channels, in_channels]
+      w = tf.get_variable('w', [k_h, k_w, output_shape[-1], input_.get_shape()[-1]],
+                initializer=tf.random_normal_initializer(stddev=stddev))
 
-    try:
-      deconv = tf.nn.conv2d_transpose(input_, w, output_shape=output_shape,
-                strides=[1, d_h, d_w, 1])
+      try:
+        deconv = tf.nn.conv2d_transpose(input_, w, output_shape=output_shape,
+                  strides=[1, d_h, d_w, 1])
 
-    # Support for verisons of TensorFlow before 0.7.0
-    except AttributeError:
-      deconv = tf.nn.deconv2d(input_, w, output_shape=output_shape,
-                strides=[1, d_h, d_w, 1])
+      # Support for verisons of TensorFlow before 0.7.0
+      except AttributeError:
+        deconv = tf.nn.deconv2d(input_, w, output_shape=output_shape,
+                  strides=[1, d_h, d_w, 1])
 
-    biases = tf.get_variable('biases', [output_shape[-1]], initializer=tf.constant_initializer(0.0))
-    deconv = tf.reshape(tf.nn.bias_add(deconv, biases), deconv.get_shape())
+      biases = tf.get_variable('biases', [output_shape[-1]], initializer=tf.constant_initializer(0.0))
+      deconv = tf.reshape(tf.nn.bias_add(deconv, biases), deconv.get_shape())
 
-    if with_w:
-      return deconv, w, biases
-    else:
-      return deconv
+      if with_w:
+        return deconv, w, biases
+      else:
+        return deconv
 
 
 # FIXME: replace by tf.nn.leaky_relu?
