ceb85bf4226344afa193ff204347658d1615705e
benckx <benoit.vleminckx@gmail.com>
2019-04-26 16:47:45 +0200

    fix video merger script (add boxes) (not tested yet)

--- scripts/merge_timecuts.py
+++ scripts/merge_timecuts.py
@@ -11,12 +11,33 @@ if len(sys.argv) > 1:
       if param == '--folder':
         folder = params[idx + 1]
 
-time_cuts = [f for f in listdir(folder) if (isfile(join(folder, f)) and f.find('_time_cut') > -1)]
-time_cuts.sort()
+time_cuts_files = [f for f in listdir(folder) if (isfile(join(folder, f)) and f.find('_time_cut') > -1)]
+time_cuts_files.sort()
+
+# for time_cut_files in time_cuts_files:
+#   print(time_cut_files)
+
+boxes_files = [f for f in listdir(folder) if (isfile(join(folder, f)) and f.find('_box') > -1)]
+boxes_files.sort()
+
+boxes_names = set()
+for box_file in boxes_files:
+  box_with_extension = box_file[box_file.find('_box') + 1:]
+
+  boxes_names.add(box_with_extension[0:box_with_extension.find('.mp4')])
+
+sorted_boxes_names = list(boxes_names)
+sorted_boxes_names.sort()
+
+for box_name in sorted_boxes_names:
+  print(box_name)
 
 merged_files_names = set()
-for time_cut in time_cuts:
-  merged_files_names.add(time_cut[0:time_cut.find('_time_cut')])
+for time_cut_files in time_cuts_files:
+    merged_files_names.add(time_cut_files[0:time_cut_files.find('_time_cut')])
+
+for merged_file_name in merged_files_names:
+  print(merged_file_name)
 
 merged_files_named_sorted = list(merged_files_names)
 merged_files_named_sorted.sort()
@@ -24,7 +45,8 @@ merged_files_named_sorted.sort()
 commands = []
 for element in merged_files_named_sorted:
   file_path = folder + '/' + element
-  commands.append('mencoder -oac copy -ovc copy ' + file_path + '_time_cut* -o ' + file_path + '.mp4')
+  for box_name in sorted_boxes_names:
+    commands.append('mencoder -oac copy -ovc copy ' + file_path + '_time_cut*' + box_name + '*' + ' -o ' + file_path + '_' + box_name + '.mp4')
 
 for command in commands:
   print(command)
