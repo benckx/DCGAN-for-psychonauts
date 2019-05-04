9443bf83fc2725e75188321bf7d5665d776b1588
benckx <benoit.vleminckx@gmail.com>
2019-05-04 17:34:17 +0200

    add offset option to cuts_renders.py

--- scripts/cuts_renders.py
+++ scripts/cuts_renders.py
@@ -28,6 +28,7 @@ def seconds_to_timestamp(seconds: int):
 def main():
   file_path = sys.argv[1]
   length = 60
+  offset = 0
   re_encode = False
 
   if len(sys.argv) > 1:
@@ -36,25 +37,42 @@ def main():
       for idx, param in enumerate(params):
         if param == '--file':
           file_path = params[idx + 1]
-        if param == '--length':
+        elif param == '--length':
           length = int(params[idx + 1])
-        if param == '--reencode':
+        elif param == '--offset':
+          offset = int(params[idx + 1])
+        elif param == '--reencode':
           re_encode = True
+        elif param.startswith('--'):
+          print('param {} not known'.format(param))
+          exit(0)
 
   file_name = file_path.split('/')[-1]
   file_folder = file_path[0:file_path.find(file_name)]
   file_name_no_extension = file_name.split('.')[0]
+  duration = get_video_duration_seconds(file_path)
+  endpos = seconds_to_timestamp(length)
+  nbr_of_bits = math.floor((duration - offset) / length)
 
+  print('length: {}'.format(length))
+  print('offset: {}'.format(offset))
   print(file_path)
   print(file_name)
   print(file_name_no_extension)
   print(file_folder)
-
-  endpos = seconds_to_timestamp(length)
+  print(duration)
+  print(nbr_of_bits)
 
   count = 1
-  for i in range(0, get_video_duration_seconds(file_path), length):
-    ss = seconds_to_timestamp(i)
+  for i in range(nbr_of_bits):
+    start = (i * length) + offset
+    end = ((i + 1) * length) + offset
+    print("bit {} --> from {} to {}".format(i, start, end))
+    count += 1
+
+  count = 1
+  for i in range(nbr_of_bits):
+    ss = seconds_to_timestamp((i * length) + offset)
     cut_num = str(count).rjust(3, '0')
     count += 1
     output_file = file_folder + file_name_no_extension + '_' + cut_num + '.mp4'
