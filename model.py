41857b5ae6facf23303ae1714599dbd45419712e
benckx <benoit.vleminckx@gmail.com>
2019-05-09 17:20:15 +0200

    revert changes made to 'model' for debugging purpose

--- model.py
+++ model.py
@@ -3,7 +3,6 @@ from __future__ import division
 import time
 from multiprocessing import Pool
 
-from dcgan_cmd_builder import Job
 from files_utils import backup_checkpoint, must_backup_checkpoint, get_checkpoint_backup_delay
 from gpu_devices import GpuAllocator
 from images_utils import DataSetManager
@@ -12,16 +11,20 @@ from utils import *
 
 frames_saving_pool = Pool(processes=20)
 
-
 def conv_out_size_same(size, stride):
   return int(math.ceil(float(size) / float(stride)))
 
 
 class DCGAN(object):
-  def __init__(self, sess, job: Job, input_height=108, input_width=108, crop=True, batch_size=64, sample_num=64,
-               output_height=64, output_width=64, y_dim=None, z_dim=100, gf_dim=64, df_dim=64, gfc_dim=1024,
-               dfc_dim=1024, input_fname_pattern='*.jpg', checkpoint_dir=None, sample_dir=None, sample_rate=None,
-               gpu_idx=None, enable_cache=True):
+  def __init__(self, sess, input_height=108, input_width=108, crop=True,
+               batch_size=64, sample_num=64, output_height=64, output_width=64,
+               grid_height=8, grid_width=8,
+               y_dim=None, z_dim=100, gf_dim=64, df_dim=64,
+               gfc_dim=1024, dfc_dim=1024, c_dim=3, dataset_names=['default'],
+               input_fname_pattern='*.jpg', checkpoint_dir=None, name='dcgan', sample_dir=None, sample_rate=None,
+               nbr_of_layers_d=5, nbr_of_layers_g=5, use_checkpoints=True, batch_norm_g=True, batch_norm_d=True,
+               activation_g=["relu"], activation_d=["lrelu"], nbr_g_updates=2, nbr_d_updates=1, gpu_idx=None,
+               enable_cache=True):
     """
 
     Args:
@@ -36,7 +39,6 @@ class DCGAN(object):
       c_dim: (optional) Dimension of image color. For grayscale input, set to 1. [3]
     """
     self.sess = sess
-    self.job = job
     self.crop = crop
 
     self.batch_size = batch_size
@@ -46,6 +48,9 @@ class DCGAN(object):
     self.input_width = input_width
     self.output_height = output_height
     self.output_width = output_width
+
+    self.grid_width = grid_width
+    self.grid_height = grid_height
 
     self.y_dim = y_dim
     self.z_dim = z_dim
@@ -59,15 +64,25 @@ class DCGAN(object):
     self.input_fname_pattern = input_fname_pattern
     self.checkpoint_dir = checkpoint_dir
 
+    self.name = name
+    self.use_checkpoints = use_checkpoints
     self.sample_dir = sample_dir
 
     self.sample_rate = sample_rate
+    self.nbr_of_layers_d = nbr_of_layers_d
+    self.nbr_of_layers_g = nbr_of_layers_g
 
-    self.activation_g = extend_array_to(self.job.activation_g, self.job.nbr_of_layers_g - 1)
-    self.activation_d = extend_array_to(self.job.activation_d, self.job.nbr_of_layers_d - 1)
+    self.batch_norm_g = batch_norm_g
+    self.batch_norm_d = batch_norm_d
 
-    print('data folders: {}'.format(self.job.dataset_folders))
-    self.data_set_manager = DataSetManager(self.job.dataset_folders, enable_cache, 'rgb')
+    self.activation_g = extend_array_to(activation_g, nbr_of_layers_g - 1)
+    self.activation_d = extend_array_to(activation_d, nbr_of_layers_d - 1)
+
+    self.nbr_g_updates = nbr_g_updates
+    self.nbr_d_updates = nbr_d_updates
+
+    print('data folders: {}'.format(dataset_names))
+    self.data_set_manager = DataSetManager(dataset_names, enable_cache, 'rgb')
 
     np.random.shuffle(self.data_set_manager.images_paths)
     imread_img = imread(self.data_set_manager.images_paths[0])
@@ -89,7 +104,6 @@ class DCGAN(object):
     print('discriminator device: {}'.format(self.gpu_allocator.discriminator_device()))
 
     self.build_model()
-    self.job_start = None
 
   def build_model(self):
     if self.y_dim:
@@ -131,14 +145,11 @@ class DCGAN(object):
           return tf.nn.sigmoid_cross_entropy_with_logits(logits=x, targets=y)
 
     with tf.device(self.gpu_allocator.discriminator_device()):
-      d_loss_real_input_tensor = sigmoid_cross_entropy_with_logits(self.D_logits, tf.ones_like(self.D),
-                                                                   self.gpu_allocator.discriminator_device())
-      d_loss_fake_input_tensor = sigmoid_cross_entropy_with_logits(self.D_logits_, tf.zeros_like(self.D_),
-                                                                   self.gpu_allocator.generator_device())
+      d_loss_real_input_tensor = sigmoid_cross_entropy_with_logits(self.D_logits, tf.ones_like(self.D), self.gpu_allocator.discriminator_device())
+      d_loss_fake_input_tensor = sigmoid_cross_entropy_with_logits(self.D_logits_, tf.zeros_like(self.D_), self.gpu_allocator.generator_device())
 
     with tf.device(self.gpu_allocator.generator_device()):
-      g_loss_input_tensor = sigmoid_cross_entropy_with_logits(self.D_logits_, tf.ones_like(self.D_),
-                                                              self.gpu_allocator.generator_device())
+      g_loss_input_tensor = sigmoid_cross_entropy_with_logits(self.D_logits_, tf.ones_like(self.D_), self.gpu_allocator.generator_device())
 
     with tf.device(self.gpu_allocator.discriminator_device()):
       self.d_loss_real = tf.reduce_mean(d_loss_real_input_tensor)
@@ -163,25 +174,23 @@ class DCGAN(object):
     with tf.device(self.gpu_allocator.other_things_device()):
       self.saver = tf.train.Saver()
 
-    self.job_start = None
-
   def train(self, config):
     print()
-    print('self.nbr_g_updates: {}'.format(self.job.nbr_g_updates))
-    print('self.nbr_d_updates: {}'.format(self.job.nbr_d_updates))
-    print('self.activation_g: {}'.format(self.job.activation_g))
-    print('self.activation_d: {}'.format(self.job.activation_d))
-    print("config.learning_rate_g: {}".format(self.job.learning_rate_g))
-    print("config.beta1_g: {}".format(self.job.beta1_g))
-    print("config.learning_rate_d: {}".format(self.job.learning_rate_d))
-    print("config.beta1_d: {}".format(self.job.beta1_d))
+    print('self.nbr_g_updates: {}'.format(self.nbr_g_updates))
+    print('self.nbr_d_updates: {}'.format(self.nbr_d_updates))
+    print('self.activation_g: {}'.format(self.activation_g))
+    print('self.activation_d: {}'.format(self.activation_d))
+    print("config.learning_rate_g: {}".format(config.learning_rate_g))
+    print("config.beta1_g: {}".format(config.beta1_g))
+    print("config.learning_rate_d: {}".format(config.learning_rate_d))
+    print("config.beta1_d: {}".format(config.beta1_d))
     print()
 
     with tf.device(self.gpu_allocator.generator_device()):
-      g_optim = adam(self.job.learning_rate_g, self.job.beta1_g).minimize(self.g_loss, var_list=self.g_vars)
+      g_optim = adam(config.learning_rate_g, config.beta1_g).minimize(self.g_loss, var_list=self.g_vars)
 
     with tf.device(self.gpu_allocator.discriminator_device()):
-      d_optim = adam(self.job.learning_rate_d, self.job.beta1_d).minimize(self.d_loss, var_list=self.d_vars)
+      d_optim = adam(config.learning_rate_d, config.beta1_d).minimize(self.d_loss, var_list=self.d_vars)
 
     try:
       tf.global_variables_initializer().run()
@@ -194,7 +203,7 @@ class DCGAN(object):
     counter = 1
     start_time = time.time()
 
-    if self.job.use_checkpoints:
+    if self.use_checkpoints:
       could_load, checkpoint_counter = self.load(self.checkpoint_dir)
       if could_load:
         counter = checkpoint_counter
@@ -205,52 +214,55 @@ class DCGAN(object):
     last_checkpoint_backup = int(time.time())
     checkpoint_backup_delay_in_min = get_checkpoint_backup_delay()
 
+    nbr_of_batches = min(len(self.data_set_manager.images_paths), config.train_size) // self.batch_size
+
     self.job_start = datetime.datetime.now()
 
     sample_z = np.random.uniform(-1, 1, size=(self.sample_num, self.z_dim))
     sample_inputs = self.data_set_manager.get_random_images(self.sample_num)
 
-    for step in xrange(self.job.get_nbr_of_steps()):
-      batch_z = np.random.uniform(-1, 1, [self.batch_size, self.z_dim]).astype(np.float32)
+    for epoch in xrange(config.epoch):
+      for idx in xrange(0, nbr_of_batches):
+        batch_z = np.random.uniform(-1, 1, [self.batch_size, self.z_dim]).astype(np.float32)
 
-      # Update D network
-      for i in range(0, self.job.nbr_d_updates):
-        batch_data = self.data_set_manager.get_random_images(self.batch_size)
-        self.sess.run([d_optim, self.d_sum], feed_dict={self.inputs: batch_data, self.z: batch_z})
+        # Update D network
+        for i in range(0, self.nbr_d_updates):
+          batch_data = self.data_set_manager.get_random_images(self.batch_size)
+          self.sess.run([d_optim, self.d_sum], feed_dict={self.inputs: batch_data, self.z: batch_z})
 
-      self.build_frame(step, i, sample_z, sample_inputs)
+        self.build_frame(i, epoch, idx, sample_z, sample_inputs)
 
-      # Update G network
-      # By default, run g_optim twice to make sure that d_loss does not go to zero (different from paper)
-      for i in range(0, self.job.nbr_g_updates):
-        self.sess.run([g_optim, self.g_sum], feed_dict={self.z: batch_z})
+        # Update G network
+        # By default, run g_optim twice to make sure that d_loss does not go to zero (different from paper)
+        for i in range(0, self.nbr_g_updates):
+          self.sess.run([g_optim, self.g_sum], feed_dict={self.z: batch_z})
 
-      self.build_frame(step, self.job.nbr_d_updates + i, sample_z, sample_inputs)
+        self.build_frame(self.nbr_d_updates + i, epoch, idx, sample_z, sample_inputs)
 
-      counter += 1
-      print("Step: [%6d] time: %4.4f" % (step, time.time() - start_time))
+        counter += 1
+        print("Epoch: [%2d] [%4d/%4d] time: %4.4f" % (epoch, idx, nbr_of_batches, time.time() - start_time))
 
-      if self.job.use_checkpoints and np.mod(counter, 500) == 2:
-        try:
-          begin = datetime.datetime.now().replace(microsecond=0)
-          self.save(config.checkpoint_dir, counter)
-          duration = datetime.datetime.now().replace(microsecond=0) - begin
-          print('duration of checkpoint saving: {}'.format(duration))
-          if must_backup_checkpoint():
-            current_time = int(time.time())
-            last_checkpoint_backup_min_ago = (current_time - last_checkpoint_backup) / 60
-            print('last checkpoint backup: {:0.2f} min. ago'.format(last_checkpoint_backup_min_ago))
-            if last_checkpoint_backup_min_ago >= checkpoint_backup_delay_in_min:
-              print('time to save the thing')
-              backup_checkpoint(self.job.name)
-              last_checkpoint_backup = int(time.time())
-            else:
-              min_before_next_backup = checkpoint_backup_delay_in_min - last_checkpoint_backup_min_ago
-              print('wait {:0.2f} more minutes before making a checkpoint backup'.format(min_before_next_backup))
-        except Exception as e:
-          print('Error during checkpoint saving: {}'.format(e))
+        if self.use_checkpoints and np.mod(counter, 500) == 2:
+          try:
+            begin = datetime.datetime.now().replace(microsecond=0)
+            self.save(config.checkpoint_dir, counter)
+            duration = datetime.datetime.now().replace(microsecond=0) - begin
+            print('duration of checkpoint saving: {}'.format(duration))
+            if must_backup_checkpoint():
+              current_time = int(time.time())
+              last_checkpoint_backup_min_ago = (current_time - last_checkpoint_backup) / 60
+              print('last checkpoint backup: {:0.2f} min. ago'.format(last_checkpoint_backup_min_ago))
+              if last_checkpoint_backup_min_ago >= checkpoint_backup_delay_in_min:
+                print('time to save the thing')
+                backup_checkpoint(self.name)
+                last_checkpoint_backup = int(time.time())
+              else:
+                min_before_next_backup = checkpoint_backup_delay_in_min - last_checkpoint_backup_min_ago
+                print('wait {:0.2f} more minutes before making a checkpoint backup'.format(min_before_next_backup))
+          except Exception as e:
+            print('Error during checkpoint saving: {}'.format(e))
 
-  def build_frame(self, step, suffix, sample_z, sample_inputs):
+  def build_frame(self, suffix, epoch, idx, sample_z, sample_inputs):
     try:
       samples, d_loss, g_loss = self.sess.run(
         [self.sampler, self.d_loss, self.g_loss],
@@ -259,8 +271,8 @@ class DCGAN(object):
           self.inputs: sample_inputs,
         },
       )
-      file_name = './{}/train_{:09d}_{:03d}.png'.format(self.sample_dir, step, suffix)
-      frames_saving_pool.apply_async(save_images, (samples, (self.job.grid_height, self.job.grid_width), file_name))
+      file_name = './{}/train_{:06d}_{:06d}_{:03d}.png'.format(self.sample_dir, epoch, idx, suffix)
+      frames_saving_pool.apply_async(save_images, (samples, (self.grid_height, self.grid_width), file_name))
       print("[Sample] d_loss: %.8f, g_loss: %.8f" % (d_loss, g_loss))
       self.log_frame_rate()
     except Exception as e:
@@ -290,7 +302,7 @@ class DCGAN(object):
         if reuse:
           scope.reuse_variables()
 
-        nbr_layers = self.job.nbr_of_layers_d
+        nbr_layers = self.nbr_of_layers_d
         print('init discriminator with ' + str(nbr_layers) + ' layers ...')
 
         # layer 0
@@ -302,7 +314,7 @@ class DCGAN(object):
           output_dim = self.df_dim * (2 ** i)
           layer_name = 'd_h' + str(i) + '_conv'
           conv_layer = conv2d(previous_layer, output_dim, name=layer_name)
-          if self.job.batch_norm_d:
+          if self.batch_norm_d:
             conv_layer = batch_norm(name='d_bn{}'.format(i))(conv_layer)
           previous_layer = add_activation(self.activation_d[i], conv_layer)
 
@@ -314,7 +326,7 @@ class DCGAN(object):
   def generator(self, z):
     with tf.device(self.gpu_allocator.generator_device()):
       with tf.variable_scope("generator") as scope:
-        nbr_layers = self.job.nbr_of_layers_g
+        nbr_layers = self.nbr_of_layers_g
         print('init generator with ' + str(nbr_layers) + ' layers ...')
 
         heights = []
@@ -336,7 +348,7 @@ class DCGAN(object):
         width = widths[nbr_layers - 1]
         z_ = linear(z, self.gf_dim * mul * height * width, 'g_h0_lin')
         prev_layer = tf.reshape(z_, [-1, heights[nbr_layers - 1], widths[nbr_layers - 1], self.gf_dim * mul])
-        if self.job.batch_norm_g:
+        if self.batch_norm_g:
           prev_layer = batch_norm(name='g_bn0')(prev_layer)
         prev_layer = add_activation(self.activation_g[0], prev_layer)
 
@@ -347,7 +359,7 @@ class DCGAN(object):
           width = widths[nbr_layers - 1 - i]
           layer_name = 'g_h' + str(i)
           prev_layer = deconv2d(prev_layer, [self.batch_size, height, width, self.gf_dim * mul], name=layer_name)
-          if self.job.batch_norm_g:
+          if self.batch_norm_g:
             prev_layer = batch_norm(name='g_bn' + str(i))(prev_layer)
           prev_layer = add_activation(self.activation_g[i], prev_layer)
 
@@ -362,7 +374,7 @@ class DCGAN(object):
       with tf.variable_scope("generator") as scope:
         scope.reuse_variables()
 
-        nbr_layers = self.job.nbr_of_layers_g
+        nbr_layers = self.nbr_of_layers_g
 
         heights = []
         widths = []
@@ -383,7 +395,7 @@ class DCGAN(object):
           linear(z, self.gf_dim * mul * heights[nbr_layers - 1] * widths[nbr_layers - 1], 'g_h0_lin'),
           [-1, heights[nbr_layers - 1], widths[nbr_layers - 1], self.gf_dim * mul])
 
-        if self.job.batch_norm_g:
+        if self.batch_norm_g:
           prev_layer = batch_norm(name='g_bn0')(prev_layer, train=False)
 
         prev_layer = add_activation(self.activation_g[0], prev_layer)
@@ -395,7 +407,7 @@ class DCGAN(object):
           w = widths[nbr_layers - i - 1]
           layer_name = 'g_h' + str(i)
           prev_layer = deconv2d(prev_layer, [self.batch_size, h, w, self.gf_dim * mul], name=layer_name)
-          if self.job.batch_norm_g:
+          if self.batch_norm_g:
             prev_layer = batch_norm(name='g_bn' + str(i))(prev_layer, train=False)
           prev_layer = add_activation(self.activation_g[i], prev_layer)
 
@@ -406,7 +418,7 @@ class DCGAN(object):
 
   @property
   def model_dir(self):
-    return self.job.name
+    return self.name
 
   def save(self, checkpoint_dir, step):
     model_name = "DCGAN.model"
