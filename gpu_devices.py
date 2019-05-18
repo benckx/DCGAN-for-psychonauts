c1481c3f8e82f7b8ee4e97a373c23987407e606c
benckx <benoit.vleminckx@gmail.com>
2019-05-18 18:42:30 +0200

    fix gpu allocation issues (allow multiple jobs on different gpus)

--- gpu_devices.py
+++ gpu_devices.py
@@ -3,37 +3,38 @@ from tensorflow.python.client import device_lib
 
 class GpuAllocator:
   def __init__(self, gpu_idx):
-    self.gpu_devices = [x for x in device_lib.list_local_devices() if x.device_type == 'GPU']
-    self.cpu_devices = [x for x in device_lib.list_local_devices() if x.device_type == 'CPU']
-    self.nbr_gpu_devices = len(self.gpu_devices)
     if gpu_idx is not None:
+      self.gpu_devices = [x for x in device_lib.list_local_devices() if x.device_type == 'GPU']
+      self.cpu_devices = [x for x in device_lib.list_local_devices() if x.device_type == 'CPU']
+      self.nbr_gpu_devices = len(self.gpu_devices)
       self.gpu_idx = int(gpu_idx)
+      for gpu_devices in self.gpu_devices:
+        print(str(gpu_devices.name))
+      for cpu_devices in self.cpu_devices:
+        print(str(cpu_devices.name))
     else:
       self.gpu_idx = None
-    for gpu_devices in self.gpu_devices:
-      print(str(gpu_devices.name))
-    for cpu_devices in self.cpu_devices:
-      print(str(cpu_devices.name))
 
     self.allocations = {}
     if self.gpu_idx is not None:
-      self.allocations['generator'] = self.gpu_idx
-      self.allocations['discriminator'] = self.gpu_idx
-      self.allocations['sampler'] = self.gpu_idx
-      self.allocations['discriminator_fake'] = self.gpu_idx
-      self.allocations['other'] = self.gpu_idx
+      device = '/device:GPU:{}'.format(gpu_idx)
+      self.allocations['generator'] = device
+      self.allocations['discriminator'] = device
+      self.allocations['sampler'] = device
+      self.allocations['discriminator_fake'] = device
+      self.allocations['other'] = device
     elif self.nbr_gpu_devices == 2:
-      self.allocations['generator'] = self.nbr_gpu_devices - 1
-      self.allocations['sampler'] = self.nbr_gpu_devices - 1
-      self.allocations['discriminator'] = self.nbr_gpu_devices - 2
-      self.allocations['discriminator_fake'] = self.nbr_gpu_devices - 2
-      self.allocations['other'] = self.nbr_gpu_devices - 2
+      self.allocations['generator'] = self.gpu_devices[self.nbr_gpu_devices - 1].name
+      self.allocations['sampler'] = self.gpu_devices[self.nbr_gpu_devices - 1].name
+      self.allocations['discriminator'] = self.gpu_devices[self.nbr_gpu_devices - 2].name
+      self.allocations['discriminator_fake'] = self.gpu_devices[self.nbr_gpu_devices - 2].name
+      self.allocations['other'] = self.gpu_devices[self.nbr_gpu_devices - 2].name
     elif self.nbr_gpu_devices >= 3:
-      self.allocations['generator'] = self.nbr_gpu_devices - 1
-      self.allocations['discriminator'] = self.nbr_gpu_devices - 2
-      self.allocations['sampler'] = self.nbr_gpu_devices - 3
-      self.allocations['discriminator_fake'] = self.nbr_gpu_devices - 3
-      self.allocations['other'] = self.nbr_gpu_devices - 3
+      self.allocations['generator'] = self.gpu_devices[self.nbr_gpu_devices - 1].name
+      self.allocations['discriminator'] = self.gpu_devices[self.nbr_gpu_devices - 2].name
+      self.allocations['sampler'] = self.gpu_devices[self.nbr_gpu_devices - 3].name
+      self.allocations['discriminator_fake'] = self.gpu_devices[self.nbr_gpu_devices - 3].name
+      self.allocations['other'] = self.gpu_devices[self.nbr_gpu_devices - 3].name
     else:
       self.allocations['generator'] = None
       self.allocations['discriminator'] = None
@@ -45,28 +46,28 @@ class GpuAllocator:
     if self.allocations['generator'] is None:
       return ''
     else:
-      return self.gpu_devices[self.allocations['generator']].name
+      return self.gpu_devices[self.allocations['generator']]
 
   def sampler_device(self):
     if self.allocations['sampler'] is None:
       return ''
     else:
-      return self.gpu_devices[self.allocations['sampler']].name
+      return self.gpu_devices[self.allocations['sampler']]
 
   def discriminator_device(self):
     if self.allocations['discriminator'] is None:
       return ''
     else:
-      return self.gpu_devices[self.allocations['discriminator']].name
+      return self.gpu_devices[self.allocations['discriminator']]
 
   def discriminator_fake_device(self):
     if self.allocations['discriminator_fake'] is None:
       return ''
     else:
-      return self.gpu_devices[self.allocations['discriminator_fake']].name
+      return self.gpu_devices[self.allocations['discriminator_fake']]
 
   def other_things_device(self):
     if self.allocations['other'] is None:
       return ''
     else:
-      return self.gpu_devices[self.allocations['other']].name
+      return self.gpu_devices[self.allocations['other']]
