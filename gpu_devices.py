d9131bfc9f4dbdb262dfee37e5c9e2143052dcdd
benckx <benoit.vleminckx@gmail.com>
2019-06-05 17:37:18 +0200

    fix the GPU allocation policy in order to be able to run multiple jobs on different GPU devices

--- gpu_devices.py
+++ gpu_devices.py
@@ -3,34 +3,30 @@ from tensorflow.python.client import device_lib
 
 class GpuAllocator:
   def __init__(self):
-    self.gpu_idx = None
-    self.gpu_devices = [x for x in device_lib.list_local_devices() if x.device_type == 'GPU']
-    self.cpu_devices = [x for x in device_lib.list_local_devices() if x.device_type == 'CPU']
-    self.nbr_gpu_devices = len(self.gpu_devices)
-    for gpu_devices in self.gpu_devices:
+    gpu_devices = [x for x in device_lib.list_local_devices() if x.device_type == 'GPU']
+    nbr_gpu_devices = len(gpu_devices)
+    for gpu_devices in gpu_devices:
       print(str(gpu_devices.name))
-    for cpu_devices in self.cpu_devices:
-      print(str(cpu_devices.name))
 
     self.allocations = {}
-    if self.nbr_gpu_devices == 1:
-      self.allocations['generator'] = self.gpu_devices[self.nbr_gpu_devices - 1].name
-      self.allocations['discriminator'] = self.gpu_devices[self.nbr_gpu_devices - 1].name
-      self.allocations['sampler'] = self.gpu_devices[self.nbr_gpu_devices - 1].name
-      self.allocations['discriminator_fake'] = self.gpu_devices[self.nbr_gpu_devices - 1].name
-      self.allocations['other'] = self.gpu_devices[self.nbr_gpu_devices - 1].name
-    elif self.nbr_gpu_devices == 2:
-      self.allocations['generator'] = self.gpu_devices[self.nbr_gpu_devices - 1].name
-      self.allocations['sampler'] = self.gpu_devices[self.nbr_gpu_devices - 1].name
-      self.allocations['discriminator'] = self.gpu_devices[self.nbr_gpu_devices - 2].name
-      self.allocations['discriminator_fake'] = self.gpu_devices[self.nbr_gpu_devices - 2].name
-      self.allocations['other'] = self.gpu_devices[self.nbr_gpu_devices - 2].name
-    elif self.nbr_gpu_devices >= 3:
-      self.allocations['generator'] = self.gpu_devices[self.nbr_gpu_devices - 1].name
-      self.allocations['discriminator'] = self.gpu_devices[self.nbr_gpu_devices - 2].name
-      self.allocations['sampler'] = self.gpu_devices[self.nbr_gpu_devices - 3].name
-      self.allocations['discriminator_fake'] = self.gpu_devices[self.nbr_gpu_devices - 3].name
-      self.allocations['other'] = self.gpu_devices[self.nbr_gpu_devices - 3].name
+    if nbr_gpu_devices == 1:
+      self.allocations['generator'] = gpu_devices[0].name
+      self.allocations['discriminator'] = gpu_devices[0].name
+      self.allocations['sampler'] = gpu_devices[0].name
+      self.allocations['discriminator_fake'] = gpu_devices[0].name
+      self.allocations['other'] = gpu_devices[0].name
+    elif nbr_gpu_devices == 2:
+      self.allocations['generator'] = gpu_devices[0].name
+      self.allocations['sampler'] = gpu_devices[0].name
+      self.allocations['discriminator'] = gpu_devices[1].name
+      self.allocations['discriminator_fake'] = gpu_devices[1].name
+      self.allocations['other'] = gpu_devices[1].name
+    elif nbr_gpu_devices >= 3:
+      self.allocations['generator'] = gpu_devices[nbr_gpu_devices - 1].name
+      self.allocations['discriminator'] = gpu_devices[nbr_gpu_devices - 2].name
+      self.allocations['sampler'] = gpu_devices[nbr_gpu_devices - 3].name
+      self.allocations['discriminator_fake'] = gpu_devices[nbr_gpu_devices - 3].name
+      self.allocations['other'] = gpu_devices[nbr_gpu_devices - 3].name
     else:
       self.allocations['generator'] = ''
       self.allocations['discriminator'] = ''
