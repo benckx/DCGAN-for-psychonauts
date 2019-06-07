957c4fab87d9a88a713355b3bbb89ca35142b933
benckx <benoit.vleminckx@gmail.com>
2019-06-07 23:38:06 +0200

    fix the GPU allocation policy in order to be able to run multiple jobs on different GPU devices / debug

--- gpu_devices.py
+++ gpu_devices.py
@@ -4,6 +4,7 @@ from tensorflow.python.client import device_lib
 class GpuAllocator:
   def __init__(self):
     gpu_devices = [x for x in device_lib.list_local_devices() if x.device_type == 'GPU']
+    print(str(gpu_devices))
     nbr_gpu_devices = len(gpu_devices)
     for gpu_devices in gpu_devices:
       print(str(gpu_devices.name))
