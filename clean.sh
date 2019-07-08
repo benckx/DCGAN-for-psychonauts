ef98af6cbf3549b2a7916117c3c61fa3512fdf65
benckx <benoit.vleminckx@gmail.com>
2019-07-08 18:34:02 +0200

    add proper logging / debug

--- clean.sh
+++ clean.sh
@@ -3,5 +3,6 @@ rm -rf samples_*
 rm -rf logs
 rm -rf checkpoint
 rm -rf *.out
+rm -rf *.log
 rm -rf *.mp4
 killall -9 python3
--- soft_clean.sh
+++ soft_clean.sh
@@ -2,4 +2,5 @@
 rm -rf samples_*
 rm -rf logs
 rm -rf *.out
+rm -rf *.log
 killall -9 python3
